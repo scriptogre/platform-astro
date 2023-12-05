4e988c845e3879e9d99434eebd3406208ed77c17
chris <106525874+scriptogre@users.noreply.github.com>
2023-12-05 12:58:39 +0200

    feat: reorganize structure and improve interactivity
    - restructure project directories, moving front-end code to a dedicated
      'frontend' folder and back-end code to a 'backend' folder
    - rename css path for base.css to improve clarity
    - add new features to frontend components, such as chat functionality and
      animations for a more engaging UX
    - integrate new backend dependencies for building a FastAPI server
    - add new astro components and templates for modular frontend development
    - implement dynamic chat messaging with GPT-3 integration
    - add Discord and Login buttons with interactive effects
    - refactor TypeScript and Tailwind CSS configuration for improved development
      experience
    - include a startup script to run both frontend and backend servers

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    safelist: ['loading', 'loading-dots', 'loading-sm'],
+    theme: {
+        fontFamily: {
+            'sans': ['Barlow', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont'], // Add other fallback fonts as required
+            'mono': ['Bebas Neue', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+        },
+        extend: {
+            boxShadow: {
+                'custom-golden': '0 0 5px #FBBD23, 0 0 25px #FBBD23, 0 0 50px #FBBD23, 0 0 100px',
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('daisyui'),
+        require('tailwindcss-animated'),
+    ],
+};
