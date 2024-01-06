9c6ac1c870428368d974fb35a330db65fd4ef0c5
chris <106525874+scriptogre@users.noreply.github.com>
2024-01-06 20:20:53 +0200

    unfinished work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import {defineConfig, passthroughImageService} from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({ applyBaseStyles: false }),
@@ -15,10 +15,11 @@ export default defineConfig({
         mdi: ['login', 'logout', 'chevron-right'],
         solar: ['arrow-left-outline', 'arrow-right-outline', 'alt-arrow-left-outline', 'alt-arrow-right-outline', 'hamburger-menu-outline'],
         ion: ['close'],
-      }
+      },
     }),
   ],
   output: "hybrid",
+  adapter: vercel(),
   image: {
     service: passthroughImageService(),
   },
