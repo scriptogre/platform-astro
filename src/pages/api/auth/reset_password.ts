f2f370cca43ee270c94488145bebd646102a7bbf
chris <106525874+scriptogre@users.noreply.github.com>
2024-01-06 23:26:20 +0200

    unfinished work

---
+++ src/pages/api/auth/reset_password.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const prerender = false
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    const formData = await request.formData();
+    const email = formData.get("email")?.toString();
+
+    if (!email) {
+        return new Response("Email is required", { status: 400 });
+    }
+
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+        redirectTo: 'https://localhost:4321',
+    })
+
+    if (error) {
+        return new Response(error.message, { status: 500 });
+    }
+
+    return redirect("/reset_password_done");
+}
